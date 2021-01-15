66b6fb62a3fb14e566f1c7143c445a9e6ff880cb
dave vebber <davevebber@gmail.com>
2021-01-14 23:32:51 -0800

    app now shuffles through five questions and alerts user of score at the end

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,35 +1,36 @@
-// start button and event listener to start game
-const startButton = document.getElementById('start-btn');
-startButton.addEventListener('click', startGame);
-
-// questions
+let score = localStorage.getItem('score');
+const scoreBtn = document.getElementById('correct-answers');
+const highscoresList = document.getElementById('highscores-list')
 const questionContainerElement = document.getElementById('question-container');
 const questionElement = document.getElementById('question')
-
-// next button
-const nextButton = document.getElementById('next-btn');
-
-// answer buttons
-const answerButtonsElement = document.getElementById('answer-buttons')
-
-// shuffle questions
 let shuffledQuestions, currentQuestionsIndex
 
+// start / next / finish / answer buttons
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const finishButton = document.getElementById('finish-btn')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+// start game function
 function startGame() {
     console.log('start game')
     startButton.classList.add('hide');
     nextButton.classList.remove('hide');
     questionContainerElement.classList.remove('hide');
+    scoreBtn.classList.remove('hide');
     currentQuestionsIndex = 0
     shuffledQuestions = questions.sort(() => Math.random() - .5)
     setNextQuestion();
+    score = 0
 };
 
+// set next question function
 function setNextQuestion() {
     resetState();
     showQuestion(shuffledQuestions[currentQuestionsIndex]);
 };
 
+// show question function
 function showQuestion(question) {
     questionElement.innerText = question.question
     question.answers.forEach(answer => {
@@ -44,39 +45,70 @@ function showQuestion(question) {
     });
 };
 
+// reset container function
 function resetState() {
+
     nextButton.classList.add('hide');
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild);
     };
 };
 
+// select answer function
 function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
 
-
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    });
+    if (shuffledQuestions.length > currentQuestionsIndex + 1) {
+        nextButton.classList.remove('hide');
+    } else {
+        nextButton.classList.add('hide');
+        finishButton.classList.remove('hide');
+    };
+    if (selectedButton.dataset = correct) {
+        score++
+    }
+    document.getElementById('correct-answers').innerHTML = "Score: " + score;
 };
 
+// change color of correct/wrong answer
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct');
 
+    } else {
+        element.classList.add('wrong');
+    };
+};
 
+// clears correct/wrong colors after each question
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
+}
 
+// finish game function
+function finishGame() {
+    document.getElementById('finish-hide').classList.add('hide');
+    document.getElementById('container').classList.add('center');
+    alert('Well done! You finished the quiz!');
+    alert("Your score is " + score);
+    console.log(score);
+}
 
+// event listeners 
+startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+    currentQuestionsIndex++;
+    setNextQuestion();
+});
+finishButton.addEventListener('click', finishGame);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// list of questions
 const questions = [
     {
         question: "Which of the following is not a coding language?",
@@ -126,14 +158,3 @@ const questions = [
         ]
     },
 ];
-
-
-
-
-
-
-
-
-
-
-
